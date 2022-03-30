f596dfc78ac16d55776f83c852c14b345edaea0a
jhp59 <jhp59@txstate.edu>
2022-03-30 15:58:57 -0500

    when user submits pdf file the content is printed to the console

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from werkzeug.utils import secure_filename
 from flask_bootstrap5 import Bootstrap
 import PyPDF2
 
-
 # UPLOAD_FOLDER = '/static/resume_uploads'
 # ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 bootstrap = Bootstrap(app)
-
 
 user = {}
 
@@ -36,15 +34,15 @@ def index():  # put application's code here
         user['name'] = text[5]
         user['email'] = text[18]
 
-        '''for i in range(len(text)):
+        for i in range(len(text)):
             # Printing the line
             # Lines are seprated using "\n"
             print(text[i], end="\n")
             # For Seprating the Pages
-        '''
+
     # closing the pdf file object
     pdfFileObj.close()
-    return render_template("index.html", user=user)
+    return render_template("index.html")
 
 
 @app.route('/upload.html')
@@ -56,6 +54,31 @@ def upload():
 def display():
     if request.method == 'POST' and 'filename' in request.files:
         file = request.files['filename']
+
+        pdfReader = PyPDF2.PdfFileReader(file)
+
+        # Getting number of pages in pdf file
+        pages = pdfReader.numPages
+        # Loop for reading all the Pages
+        for i in range(pages):
+            # Creating a page object
+            pageObj = pdfReader.getPage(i)
+            # Printing Page Number
+            print("Page No: ", i)
+            # Extracting text from page
+            # And splitting it into chunks of lines
+            text = pageObj.extractText().split('\n')
+            # Finally the lines are stored into list
+            # For iterating over list a loop is used
+            # user['name'] = text[5]
+            # user['email'] = text[18]
+
+            for j in range(len(text)):
+                # Printing the line
+                # Lines are separated using "\n"
+                print(text[j], end="\n")
+                # For Separating the Pages
+
         # process more info about file
         print("file sent successfully")
 

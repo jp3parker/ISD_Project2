52bbdd2fe18fa66dca6258efb4c9f92f264f9467
jhp59 <jhp59@txstate.edu>
2022-03-31 22:15:25 -0500

    done parsing pdf and displaying parsed content

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 from flask_bootstrap5 import Bootstrap
 import PyPDF2
 
@@ -42,23 +42,46 @@ def display():
             text = pageObj.extractText().split('\n')
             # Finally the lines are stored into list
             # For iterating over list a loop is used
-            user['name'] = text[5] + text[6] + text[7] + text[8]
-            print("name = " + user['name'])
-            user['email'] = text[18]
-            print("email = " + user['email'])
 
+            # the join function joins the strings together so
+            # ''.join(text[5:9]) is effectively the same as
+            # text[5]+text[6]+text[7]+text[8]
+            user['name'] = ''.join(text[5:9])
+            user['address'] = ''.join(text[10:13])
+            user['phone_number'] = ''.join(text[14:17])
+            user['email'] = text[18]
+            user['qualifications'] = ''.join(text[23:36])
+            user['education'] = ''.join(text[40:54])
+            user['accomplishments'] = ''.join(text[58:106])
+            user['work_history'] = ''.join(text[110:132])
+            user['professional_affiliation'] = ''.join(text[136:139])
+            user['computer_skills'] = ''.join(text[146:151])
+
+            '''
             for j in range(len(text)):
                 # Printing the line
                 # Lines are separated using "\n"
-                print(text[j], end="\n")
+                print("page #" + str(j) + ": " + text[j], end="\n")
                 # For Separating the Pages
+            '''
 
-        # process more info about file
         print("file sent successfully")
+        return render_template("display.html", name=user['name'], address=user['address'],
+                               phone_number=user['phone_number'], email=user['email'],
+                               qualifications=user['qualifications'], education=user['education'],
+                               accomplishments=user['accomplishments'], work_history=user['work_history'],
+                               professional_affiliation=user['professional_affiliation'],
+                               computer_skills=user['computer_skills'])
+
     else:
         print("something is wrong")
-        return '<br><p style="color: red">Make sure you are submitting a pdf file!</p>'
-    return render_template("display.html")
+        return '<p style="color: red;margin: 96px;">Something went wrong. ' \
+               'Make sure you are submitting a pdf file!</p>'
+
+
+@app.route('/submit.html', methods=['POST'])
+def submit():
+    return render_template("submit.html")
 
 
 if __name__ == '__main__':
